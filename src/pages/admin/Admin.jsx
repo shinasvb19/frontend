35265a69acb46f2a154c000d10b5fefb1fff601e
shinasvb19 <myofficial.shinas@gmail.com>
2023-01-25 14:30:40 +0530

    small_err

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import instance from "../../app/api/instance";
 import Sidebar from "../../components/admin/Sidebar";
-import UserManagementTable from "../../components/dataTable/UserManagementTable";
-import { logOutAdmin } from "../../features/admin/adminMainSlice";
+// import UserManagementTable from "../../components/dataTable/UserManagementTable";
+// import { logOutAdmin } from "../../features/admin/adminMainSlice";
 
 const Admin = () => {
   const dispatch = useDispatch();
@@ -20,7 +20,7 @@ const Admin = () => {
       <Sidebar />
       <div className="w-full h-screen">
         <div className="mx-auto max-w-[1200px] bg-white mt-20 rounded-3xl p-8">
-          <UserManagementTable data={data} load={load} change={setLoad} />
+          {/* <UserManagementTable data={data} load={load} change={setLoad} /> */}
         </div>
       </div>
     </div>
